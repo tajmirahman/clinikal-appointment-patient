35f97d39e7e7baf62b3acfd5cc3dc90133dd4b89
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-12-29 21:46:04 +0600

    footer section is completed

--- my-project/src/Layouts/HomeLayout.jsx
+++ my-project/src/Layouts/HomeLayout.jsx
@@ -1,11 +1,19 @@
 
+import Footer from '../Components/Footer';
 import Navber from '../Components/Navber';
 
 const HomeLayout = () => {
     return (
         <div className=''>
             {/* Navber section  */}
-            <Navber></Navber>
+                <Navber></Navber>
+            
+
+
+
+            {/* footer */}
+            <Footer></Footer>
+            
         </div>
     );
 };
