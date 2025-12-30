40eb677e42b0c9d53ef70c92c6a136125c5e018e
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-12-31 00:11:21 +0600

    who we are page is working in 80 %

--- my-project/src/Pages/WhoWeAre/SingleList.jsx
+++ my-project/src/Pages/WhoWeAre/SingleList.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
 
-const SingleList = ({list}) => {
+
+const SingleList = ({ list }) => {
+
 
     return (
         <div>
-            <div>
-                
+            <div className="text-4xl text-primary">
+                <img className="w-16 h-16" src={list.icon} alt="" />
             </div>
             <div>
                 <h2>{list.count}</h2>
--- my-project/src/Pages/WhoWeAre/WhoWeAre.jsx
+++ my-project/src/Pages/WhoWeAre/WhoWeAre.jsx
@@ -7,22 +7,35 @@ import SingleList from './SingleList';
 const WhoWeAre = () => {
 
     const [clinicList, setClinicList] = useState([]);
+    const [description, setDescription] = useState('');
 
     useEffect(() => {
         fetch('/whatweare.json')
             .then(res => res.json())
-            .then(data => setClinicList(data.stats))
+            .then(data => {
+                setClinicList(data.stats)
+                setDescription(data.description)
+            })
+
     }, [])
 
     console.log(clinicList)
 
     return (
-        <div className='w-9/12 mx-auto flex justify-between items-center gap-3'>
-            <h2>What We Do</h2>
-            <div>
-                {
-                    clinicList.map(list=><SingleList key={list.id} list={list}></SingleList>)
-                }
+        <div className='w-9/12 mx-auto '>
+            <h2 className='text-center text-3xl'>What We Do</h2>
+            <div className='flex gap-3'>
+                <div>
+                    <div className='grid grid-cols-2'>
+                        {
+                            clinicList.map(list => <SingleList key={list.id} list={list}></SingleList>)
+                        }
+                    </div>
+                    <p>{description}</p>
+                </div>
+                <div>
+                    <img src={WhatWeAre} alt="" />
+                </div>
             </div>
         </div>
     );
