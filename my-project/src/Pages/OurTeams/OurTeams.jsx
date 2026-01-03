d714603c64e0803570fe796b22333125ae6b5e32
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2026-01-03 16:00:07 +0600

    our doctor list is working

--- my-project/src/Pages/OurTeams/OurTeams.jsx
+++ my-project/src/Pages/OurTeams/OurTeams.jsx
@@ -2,15 +2,35 @@ import { useEffect, useState } from "react";
 import SingleDoctor from "./SingleDoctor";
 
 
+
 const OurTeams = () => {
 
-    const [ourTeam, setOurTeam]=useState([]);
+    const [ourTeam, setOurTeam] = useState([]);
+    const [startIndex, setStartIndex] = useState(0);
 
-    useEffect(()=>{
-        fetch ('/ourTeam.json')
-       .then((res)=>res.json())
-       .then(data=> setOurTeam(data)) 
-    },[])
+
+    useEffect(() => {
+        fetch('/ourTeam.json')
+            .then((res) => res.json())
+            .then(data => setOurTeam(data))
+    }, [])
+
+
+    useEffect(() => {
+        const slider = setInterval(() => {
+            setStartIndex((prev) =>
+                prev + 1 > ourTeam.length - 6 ? 0 : prev + 1
+            );
+        }, 3000);
+
+        return () => clearInterval(slider);
+    }, [ourTeam.length]);
+
+    const visibleDoctors = ourTeam.slice(startIndex, startIndex + 3);
+
+
+
+
 
 
     return (
@@ -18,13 +38,18 @@ const OurTeams = () => {
             <h2 className='text-3xl text-center'>Our Teams</h2>
             <hr className="w-2/6 mx-auto border-t-2 border-black mt-2" />
 
-            <div>
-                {
-                    ourTeam.map(singleDoctor=><SingleDoctor
-                    key={singleDoctor.id}
-                    singleDoctor={singleDoctor}
-                    ></SingleDoctor>)
-                }
+            <div className="w-full min-h-screen flex items-center justify-center bg-gradient-to-r from-[#2fa4aa] to-[#5fd1d8]">
+                <div className="flex gap-8 items-center">
+                    {
+                        visibleDoctors.map((doc, index) => {
+                            const isActive = index === startIndex;
+                            return (
+                                <SingleDoctor doc={doc} isActive={isActive}></SingleDoctor>
+
+                            );
+                        })
+                    }
+                </div>
             </div>
         </div>
     );
--- my-project/src/Pages/OurTeams/SingleDoctor.jsx
+++ my-project/src/Pages/OurTeams/SingleDoctor.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 
-const SingleDoctor = ({singleDoctor}) => {
+const SingleDoctor = ({ doc, isActive }) => {
     return (
         <div>
-            <div className="card">
+            {/* <div className="card">
                 <img src={singleDoctor.img} alt="" />
                 <h2>{singleDoctor.doctor_name}</h2>
                 <h3>{singleDoctor.specialist}</h3>
+            </div> */}
+
+            <div
+              key={doc.id}
+              className={`transition-all duration-700 rounded-xl overflow-hidden shadow-xl
+                ${isActive
+                  ? "w-72 bg-white scale-110"
+                  : "w-60 bg-white/70 scale-90 blur-[1px]"
+                }`}
+            >
+              <img
+                src={doc.img}
+                alt={doc.name}
+                className="h-64 w-full object-cover"
+              />
+
+              <div className="p-5 text-center">
+                <h2 className="text-lg font-semibold">{doc.name}</h2>
+                <p className="text-sm text-gray-500">{doc.dept}</p>
+              </div>
             </div>
+
         </div>
     );
 };
