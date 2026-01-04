a45f56fba572a538f9204f9a038aea44a8d8450a
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2026-01-04 17:31:15 +0600

    image did not found from newpost

--- my-project/src/Pages/LatestPost/LatestPost.jsx
+++ my-project/src/Pages/LatestPost/LatestPost.jsx
@@ -4,22 +4,26 @@ import LatestPostCard from "./LatestPostCard";
 
 const LatestPost = () => {
 
-    const [latestPost, setLatestPost]=useState([]);
+    const [latestPost, setLatestPost] = useState([]);
 
-    useEffect(()=>{
-        fetch ('/latestPost.json')
-        .then(res=>res.json())
-        .then(data=> setLatestPost(data))
-    },[])
-    
+    useEffect(() => {
+        fetch('/latestPost.json')
+            .then(res => res.json())
+            .then(data => setLatestPost(data))
+    }, [])
+
     return (
         <div>
-           {
-            latestPost.map(newPost=><LatestPostCard 
-                key={newPost.id} 
-                newPost={newPost}
-                ></LatestPostCard>)
-           }
+            <h2 className='text-3xl text-center'>Latest Post</h2>
+            <hr className="w-[200px] mx-auto border-t-2 border-black" />
+            <div>
+                {
+                    latestPost.map(newPost => <LatestPostCard
+                        key={newPost.id}
+                        newPost={newPost}
+                    ></LatestPostCard>)
+                }
+            </div>
         </div>
     );
 };
--- my-project/src/Pages/LatestPost/LatestPostCard.jsx
+++ my-project/src/Pages/LatestPost/LatestPostCard.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 
-const LatestPostCard = ({newPost}) => {
+const LatestPostCard = ({ newPost }) => {
+    console.log(newPost)
     return (
         <div className="card">
-           <figure>
-            <img src="" alt="" />
-           </figure>
+            <figure>
+                <img src={newPost.image} alt="" />
+            </figure>
 
-           <div>
-            data
-           </div>
+            {/* <div>
+                data
+            </div> */}
 
-           <div>
-            <h2></h2>
-            <div>
-                <button></button>
-                <button></button>
-            </div>
-           </div>
+            {/* <div>
+                <h2>{newPost.title}</h2>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                        <p>{newPost.author}</p>
+                    </div>
+                    <div>
+                        <button>read more</button>
+                    </div>
+                </div>
+            </div> */}
 
-           
+
         </div>
     );
 };
