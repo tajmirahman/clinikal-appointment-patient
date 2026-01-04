b805540cc4ae4d8d69e9557096351242d319522b
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2026-01-04 17:10:15 +0600

    latest post work is going

--- my-project/src/Pages/LatestPost/LatestPost.jsx
+++ my-project/src/Pages/LatestPost/LatestPost.jsx
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react";
 
 
 const LatestPost = () => {
+
+    const [latestPost, setLatestPost]=useState([]);
+
+    useEffect(()=>{
+        fetch ('/latestPost.json')
+        .then(res=>res.json())
+        .then(data=> setLatestPost(data))
+    },[])
     
     return (
         <div>
